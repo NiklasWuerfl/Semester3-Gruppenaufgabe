28ecfb980678dc36252c52d7ddde19ba00f772a5
daniela1911 <116259666+daniela1911@users.noreply.github.com>
2022-11-01 13:42:00 +0100

    Update Frontend.py

--- Frontend.py
+++ Frontend.py
@@ -16,18 +16,23 @@ def LoginSeite():
         *
     """
     
-    layout = [[sg.Text('Username:'), sg.InputText()],
+    layout = [[sg.Text('Login', font=('any', 12, 'bold'))],
+          [sg.Text('Username:'), sg.InputText()],
           [sg.Text('Passwort:'), sg.InputText()],
           [sg.Button('Anmelden')]]
 
-    window=sg.Window('Login Seite', layout)
+    window=sg.Window('Studierendenverwaltungssystem', layout)
 
     while True:
         event, values= window.read()
         if event == sg.WIN_CLOSED:
             break
+        elif event == "Anmelden":
+            break
 
     window.close()
+
+
 
 def FalseLoginSeite():
     """Implimentierung der Seite nach falschem einloggen
@@ -37,18 +42,59 @@ def FalseLoginSeite():
         *
     """
     
-    layout = [[sg.Text('Username oder Passwort falsch', text_color= 'red')],
+    layout = [[sg.Text('Login', font=('any', 12, 'bold'))],
+        [sg.Text('Username oder Passwort falsch', text_color= 'red')],
         [sg.Text('Username:'), sg.InputText()],
         [sg.Text('Passwort:'), sg.InputText()],
         [sg.Button('Anmelden')],
         [sg.Text('Falls Sie das Passwort vergessen haben, wenden Sie sich an den Administrator!')]]
 
-    window=sg.Window('Login Seite', layout)
+    window=sg.Window('Studierendenverwaltungssystem', layout)
 
     while True:
         event, values= window.read()
         if event == sg.WIN_CLOSED:
             break
+        elif event == "Anmelden":
+            break
 
     window.close()
+
+def StudierendenSeite():
+    """Seite für Studierende für die Einsicht der Noten
+
+        Tests:
+        *
+        *
+    """
+    information_array=[
+    ['Programmierung', 1, 5.0, 120, 100, 1.6, True],
+    ['Mathe 1', 1, 5.0, 120, 75, 2.5, True]]
+
+    headings=['Modul', 'Versuch', 'Cedits', 'P. g.', 'P. e.', 'Note', 'best.']
+    
+    layout = [[sg.Text('Herzlich Willkommen!'), sg.Button('Passwort ändern', font=('any', 9, 'underline')), sg.Button('Abmelden', font=('any', 9, 'underline'))],
+          [sg.Text('Leistungsübersicht', font=('any', 12, 'bold'))],
+          [sg.Table(values=information_array, headings=headings, max_col_width=35,
+                    auto_size_columns=True,
+                    display_row_numbers=True,
+                    justification='left',
+                    num_rows= 5,
+                    key= '-Table-',
+                    row_height=35)],
+          [sg.Text('Gesamt', font=('any', 12, 'bold')), sg.Text('Cedits gesamt'), sg.Text('GPA')]]
+          
+    window=sg.Window('Studierendenverwaltungssystem', layout)
+
+    while True:
+        event, values= window.read()
+        if event == sg.WIN_CLOSED:
+            break
+        elif event == "Passwort ändern":
+            break
+        elif event == "Abmelden":
+            break
+
+    window.close()
+
 
